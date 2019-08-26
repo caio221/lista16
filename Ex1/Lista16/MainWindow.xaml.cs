e9940f8a881891fe409f821f0016577ec97d8324
Caio Vinicius de Azevedo <vinicius.caio@academico.ifrn.edu.br>
2019-08-26 13:46:48 -0300

    Atividade 3

---
+++ Ex1/Lista16/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lista16
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Retangulo x = new Retangulo();
+            x.SetRaio(double.Parse(R.Text));
+            A.Text = x.CalcArea().ToString();
+            V.Text = x.CalcVolume().ToString();
+        }
+    }
+}
---
+++ Ex1/Lista16/Retangulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista16
+{
+    class Retangulo //fiz errado aqui, o nome era pra ser esfera... to com medo de mudar e bugar tudo entao vou deixar assim
+    {
+        private double raio;
+        public void SetRaio(double v) { if (v > 0) raio = v; }
+        public double GetRaio() { return raio; }
+        public double CalcArea() { return 4 * 3.14 * (raio * raio); }
+        public double CalcVolume() { return 4 / 3 * 3.14 * (raio * raio * raio); }
+    }
+}
